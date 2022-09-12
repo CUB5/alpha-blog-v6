d8a0b032e30210180a29a3eef0bc6f32136edb2b
Javier Sanchez <francisco.sanchez@digital55.com>
2022-09-12 10:41:38 +0200

    cambios actualizados

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def show
+    #@article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    #@article = Article.find(params[:id])
+  end
+
+  def create
+    #render plain: params[:article]
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Articulo creado satisfactoriamente'
+    #redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    #@article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Articulo actualizado satisfactoriamente'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+end
