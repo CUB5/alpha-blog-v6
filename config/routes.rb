ae613011e00e5361196b751edf2e3350494cc9f4
Javier Sanchez <francisco.sanchez@digital55.com>
2022-09-15 14:26:05 +0200

    Creadas rutas de sesion y formulario de inicio de sesion

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   #post 'users', to: 'users#create'
   resources :users, except: [:new]
+  #vista iniciar sesion
+  get 'login', to: 'sessions#new'
+  #validar inicio de sesion
+  post 'login', to: 'sessions#create'
+  #cerrar sesion
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
