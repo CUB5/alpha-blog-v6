b7124316b4545fa1b8d6c007edc38d59d245f3a0
Javier Sanchez <francisco.sanchez@digital55.com>
2022-09-15 12:41:27 +0200

    actualizado vistas articulos y usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Información actualizada satisfactoriamente'
+      #redirect_to article_path(@article)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+
   end
 
   def create
