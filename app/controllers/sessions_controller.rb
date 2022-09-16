94ebd2a98ed6a6199622889ec86e737641694832
Javier Sanchez <francisco.sanchez@digital55.com>
2022-09-16 10:24:34 +0200

    autentificacion de usuarios

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,25 @@
 class SessionsController < ApplicationController
 
   def new
-    
+
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Autentificado correctamente"
+      redirect_to user
+    else
+      flash.now[:alert] = "Error en las credenciales"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Desconectado"
+    redirect_to root_path
   end
 
 end
