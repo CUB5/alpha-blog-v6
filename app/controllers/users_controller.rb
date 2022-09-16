b58187e47760de6f6b3c04682b5e0ceef5b74227
Javier Sanchez <francisco.sanchez@digital55.com>
2022-09-16 13:25:36 +0200

    Finalizadas restricciones usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    @articles = @user.articles
+    #@articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.all
+    #@users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -39,6 +41,14 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    # @user = User.find(params[:id])
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "La cuenta y todos sus articulos relacionas eliminados correctamente"
+    redirect_to root_path
   end
 
   private
